c26b2ac04a00b895c6c93d120287e26fee0de6ae
FRex <rex2k8@gmail.com>
2019-04-23 05:26:30 +0200

    member vars for widget ptrs

--- src/BlaxorApp.hpp
+++ src/BlaxorApp.hpp
@@ -4,6 +4,9 @@
 #include <string>
 
 class Fl_Double_Window;
+class Fl_Box;
+class BlaHexDisplay;
+class Fl_Slider;
 
 class BlaxorApp : BlaNonCopyable
 {
@@ -16,5 +19,8 @@ private:
     BlaHexFile m_file;
     std::string m_wintitle;
     Fl_Double_Window * m_win = 0x0;
+    Fl_Box * m_box = 0x0;
+    BlaHexDisplay * m_display = 0x0;
+    Fl_Slider * m_slider = 0x0;
 
 };
