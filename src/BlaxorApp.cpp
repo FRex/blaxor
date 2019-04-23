89f194141570d6c823d26fcc82b691b0784cc264
FRex <rex2k8@gmail.com>
2019-04-23 05:32:50 +0200

    typo fix

--- src/BlaxorApp.cpp
+++ src/BlaxorApp.cpp
@@ -53,15 +53,15 @@ void BlaxorApp::setupGui()
 {
     const int w = 900;
     const int h = 600;
-    const int scrollballw = 20;
+    const int scrollbarw = 20;
     const int boxh = 50;
     m_win = new Fl_Double_Window(w, h, m_wintitle.c_str());
     m_box = new Fl_Box(0, 0, w, boxh);
     m_box->callback(&update_label_cb, &m_file);
     m_box->do_callback();
     Fl::add_timeout(1.0, &update_label_to, m_box);
-    m_slider = new Fl_Slider(w - scrollballw, boxh, scrollballw, h - boxh);
-    m_display = new BlaHexDisplay(0, boxh, w - scrollballw, h - boxh);
+    m_slider = new Fl_Slider(w - scrollbarw, boxh, scrollbarw, h - boxh);
+    m_display = new BlaHexDisplay(0, boxh, w - scrollbarw, h - boxh);
     m_win->resizable(m_display);
     m_display->take_focus();
     m_display->setFile(&m_file);
