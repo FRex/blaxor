9ec602b3e7dde1919b7fe703868ed77cbe3208d0
FRex <rex2k8@gmail.com>
2019-04-29 17:15:08 +0200

    added windows subsystem winmain calling wmain

--- src/main.cpp
+++ src/main.cpp
@@ -11,3 +11,18 @@ int wmain(int argc, wchar_t ** argv)
     app.setupGui();
     return Fl::run();
 }
+
+//call out wmain with right args using subsystem:windows entry point
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
+{
+    int argc = 0;
+    wchar_t ** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+    if(!argv)
+        return 1;
+
+    const int ret = wmain(argc, argv);
+    LocalFree(argv);
+    return ret;
+}
