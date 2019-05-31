df723c5fc80b5e5c5a96dd2f5d2e1fae5980c648
FRex <rex2k8@gmail.com>
2019-05-31 20:03:07 +0200

    basic splitting box display things by comma

--- src/BlaBox.cpp
+++ src/BlaBox.cpp
@@ -24,21 +24,46 @@ static std::vector<std::string> split(const std::string& str)
     return ret;
 }
 
+std::vector<std::string> splitbycomma(std::string text)
+{
+    for(char& c : text)
+        if(c == ',')
+            c = '\n';
+
+    return split(text);
+}
+
 void BlaBox::draw()
 {
     draw_box();
     if(label() == 0x0)
         return;
 
-    const auto lines = split(label());
     fl_color(labelcolor());
     fl_font(labelfont(), labelsize());
-    for(int i = 0; i < static_cast<int>(lines.size()); ++i)
+
+    const auto parts = splitbycomma(label());
+    int line = 0;
+    int wsofar = 0;
+    const int ww = w();
+
+    Fl_Color c1 = FL_BLACK;
+    Fl_Color c2 = FL_RED;
+    for(const std::string& p : parts)
     {
-        const int yy = y() + fl_height() - fl_descent() + i * fl_height();
-        const int ww = bla_text_width(lines[i].c_str());
-        const int xx = x() + std::max<int>(0, (w() - ww) / 2);
-        fl_draw(lines[i].c_str(), xx, yy);
+        const int tw = bla_text_width(p.c_str());
+        if(tw + wsofar > ww)
+        {
+            ++line;
+            wsofar = 0;
+        }
+
+        const int yy = y() + fl_height() - fl_descent() + line * fl_height();
+        const int xx = x() + wsofar;
+        std::swap(c1, c2);
+        fl_color(c1);
+        fl_draw(p.c_str(), xx, yy);
+        wsofar += tw;
     }
 }
 
